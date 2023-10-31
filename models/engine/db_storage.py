ca264719e2825837e48a0bd39bbcfbb7f95e784d
AchrafEs <achraf.es2110@gmail.com>
2023-10-31 13:00:48 +0000

    updated the db storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,21 +77,22 @@ class DBStorage:
 
     def get(self, cls, id):
         """
-        Retrieves an object from file storage by name or ID.
+        Returns the object based on the class name and its ID, or None if not
+        found
         """
         objects = self.__session.query(classes[cls])
         for obj in objects:
             if obj.id == id:
                 return obj
-
         return None
 
     def count(self, cls=None):
         """
-        counts the number of objcts in storge matching the given class
+        Returns the number of objects in storage matching the given class name.
+        If no name is passed, returns the count of all objects in storage.
         """
         nobjects = 0
         for clss in classes:
-            if cls is None or cls is classes[clss]:
+            if cls is None or cls is classes[clss] or cls is clss:
                 nobjects += len(self.__session.query(classes[clss]).all())
         return nobjects
